ae3160554f7441ee4d6b81d0e839a6ee8aaea358
Manoj Kumar <manoj6932@gmail.com>
2024-08-30 15:39:07 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void insert(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+head = newNode;
+    }
+    else {
+        struct Node temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void display(struct Node head) {
+    struct Node temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insert(&head, 1);
+    insert(&head, 2);
+    insert(&head, 3);
+    insert(&head, 4);
+    insert(&head, 5);
+
+    display(head);
+
+    // Bug: Deleting the head node without updating the head pointer
+    free(head);
+
+    display(head);
+
+    return 0;
+}
